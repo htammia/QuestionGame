aa7c029dc8f2ab02bbaa4487fa001f5bc4517d36
Henna Tammia <htammia20@student.oulu.fi>
2023-07-28 08:37:04 +0300

    fix link to neverever

--- scripts/index_script.js
+++
@@ -1,8 +0,0 @@
-const truth_button = document.getElementById("truths");
-const rather_button = document.getElementById("rather");
-const never_button = document.getElementById("never");
-
-never_button.addEventListener("click", function(){
-    console.log("hi");
-    window.location.href ="/neverever.html";
-});
